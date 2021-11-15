126b679928e079f06fbffcf0a13d1989df78d433
Seenu K <iitm.seenuk@gmail.com>
2021-11-15 16:58:10 +0530

    Add Book Model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=200)
+    authors = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    number_of_pages = models.PositiveIntegerField()
+    publisher = models.CharField(max_length=200)
+    release_date = models.DateField()
+
+    def __str__(self):
+        return self.name
